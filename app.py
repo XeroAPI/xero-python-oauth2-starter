592919020a945e4cd1a0a37fc4e4917689197959
Jan Nakladal <mojeto1@gmail.com>
2020-03-05 00:10:26 +1300

    Use correct oauth2 client application class.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import json
 
 from flask import Flask, url_for, render_template, session, redirect
-from flask_oauthlib.client import OAuth, OAuthException
+from flask_oauthlib.contrib.client import OAuth, OAuth2Application
 from flask_session import Session
 
 app = Flask(__name__)
@@ -14,18 +14,46 @@ Session(app)
 oauth = OAuth(app)
 xero = oauth.remote_app(
     name="xero",
-    consumer_key=app.config["CLIENT_ID"],
-    consumer_secret=app.config["CLIENT_SECRET"],
-    base_url="https://api.xero.com/",
-    authorize_url="https://login.xero.com/identity/connect/authorize",
+    version="2",
+    client_id=app.config["CLIENT_ID"],
+    client_secret=app.config["CLIENT_SECRET"],
+    endpoint_url="https://api.xero.com/",
+    authorization_url="https://login.xero.com/identity/connect/authorize",
     access_token_url="https://identity.xero.com/connect/token",
-)
+    refresh_token_url="https://identity.xero.com/connect/token",
+    scope="offline_access openid profile email accounting.transactions",
+)  # type: OAuth2Application
+
+
+@xero.tokengetter
+def obtain_xero_token():
+    return session.get("token")
+
+
+@xero.tokensaver
+def store_xero_token(token):
+    session["token"] = token
+    session.modified = True
 
 
 @app.route("/")
 def index():
-    xero_access = session.get("xero_access") or {}
-    return render_template("index.html", title="Home", xero=json.dumps(xero_access))
+    xero_access = dict(obtain_xero_token() or {})
+    return render_template(
+        "index.html",
+        title="Home",
+        token=json.dumps(xero_access, sort_keys=True, indent=4),
+    )
+
+
+@app.route("/connections")
+def tenants():
+    response = xero.get("/connections")
+    return render_template(
+        "tenants.html",
+        title="Xero Tenants",
+        tenants=json.dumps(response.json(), sort_keys=True, indent=4),
+    )
 
 
 @app.route("/login")
@@ -36,7 +64,7 @@ def login():
     }
     redirect_url = url_for("oauth_callback", _external=True)
     print(redirect_url)
-    response = xero.authorize(callback=redirect_url, **params)
+    response = xero.authorize(callback_uri=redirect_url)
     print(response.headers["Location"])
     return response
 
@@ -45,20 +73,31 @@ def login():
 def oauth_callback():
     try:
         response = xero.authorized_response()
-    except OAuthException as e:
+    except Exception as e:
         print(e)
         raise
     # todo validate state value
     if response is None or response.get("access_token") is None:
         return "Access denied: response=%s" % response
-    session["xero_access"] = dict(response)
-    session.modified = True
+    store_xero_token(response)
     return redirect(url_for("index", _external=True))
 
 
 @app.route("/logout")
 def logout():
     session["xero_access"] = None
+    session.modified = True
+    return redirect(url_for("index", _external=True))
+
+
+@app.route("/refresh_token")
+def oauth_refresh_token():
+    try:
+        result = xero.generate_request_token()
+    except Exception as e:
+        print(e)
+        raise
+    session["xero_refresh"] = result
     session.modified = True
     return redirect(url_for("index", _external=True))
 
